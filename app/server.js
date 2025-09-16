3d3b0660e098f469fd7d9758509d9d768c2b1abf
rudra1in <rudra1in@yahoo.com>
2025-09-16 21:58:35 +0530

    sample nodejs with docker compose

---
+++ app/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node.js!');
+});
+app.listen(port, () => {
+  console.log(`Node.js app listening at http://localhost:${port}`);
+});
